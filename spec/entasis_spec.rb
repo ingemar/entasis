d8cd4960bd2e754f3f0ef15160421495e36a6545
ingemar <ingemar@elabs.se>
2010-10-26 16:29:56 +0200

    Removing trailing spaces

--- spec/entasis_spec.rb
+++ spec/entasis_spec.rb
@@ -11,17 +11,17 @@ describe "Entasis::Base" do
       hilda.city.should == 'Berlin'
     end
   end
-  
+
   describe "#attribute_names" do
     it 'returns a list of attribute names' do
       hilda.attribute_names.should == %w[name age city]
     end
-    
+
     it 'is an alias for #members' do
       Person.instance_method(:attribute_names).should == Person.instance_method(:members)
     end
   end
-  
+
   describe "#attributes" do
     it 'returns a hash of attributes names and values' do
       hilda.attributes.should == {"name"=>"Hilda", "age"=>23, "city"=>"Berlin"}
--- spec/person.rb
+++ spec/person.rb
@@ -1,6 +1,6 @@
 class Person < Struct.new(:name, :age, :city)
   include Entasis::Base
-  
+
   def age=(years)
     self['age'] = years.to_i
   end
